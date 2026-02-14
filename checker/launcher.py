80442eef32940aa6b5386b2f12ed51895c957927
mrgrayyooo <jf9kzggkxv@privaterelay.appleid.com>
2026-02-14 17:58:50 +0200

    Update launcher.py

--- checker/launcher.py
+++ checker/launcher.py
@@ -17,7 +17,7 @@ from typing import List, Optional, Tuple
 OUTPUT_FILE = "best_nodes.txt"
 XRAY_PATH = "./core/xray"
 MAX_CHECK = 3000
-FINAL_LIMIT = 30
+FINAL_LIMIT = 150
 CONCURRENCY = 30
 SPEED_LIMIT = float(os.getenv("SPEED_LIMIT", 0.5))  # Мбит/с
 TEST_URL = "https://speed.cloudflare.com/__down?bytes=10000000"
