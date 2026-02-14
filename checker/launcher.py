97ddf0615505a6a95aebf537ebfdf1c1c13cce96
mrgrayyooo <jf9kzggkxv@privaterelay.appleid.com>
2026-02-14 22:20:52 +0200

    Update launcher.py

--- checker/launcher.py
+++ checker/launcher.py
@@ -18,9 +18,9 @@ from typing import List, Optional, Tuple
 OUTPUT_FILE = "best_nodes.txt"
 XRAY_PATH = "./core/xray"
 MAX_CHECK = 3000
-FINAL_LIMIT = 100
-CONCURRENCY = 10
-SPEED_LIMIT = float(os.getenv("SPEED_LIMIT", 0.3))
+FINAL_LIMIT = 10
+CONCURRENCY = 15
+SPEED_LIMIT = float(os.getenv("SPEED_LIMIT", 0.5))
 TEST_URL = "https://speed.cloudflare.com/__down?bytes=10000000"
 TCP_PING_TIMEOUT = 3
 MMDB_URL = "https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-Country.mmdb"
@@ -36,9 +36,9 @@ logger = logging.getLogger("checker")
 
 # ------------------ Источники ------------------
 SOURCES = [
-    "https://raw.githubusercontent.com/Danialsamadi/v2go/refs/heads/main/Splitted-By-Country/PL.txt",
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/Vless-Reality-White-Lists-Rus-Mobile.txt",
-    "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-SNI-RU-all.txt"
+    "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-SNI-RU-all.txt",
+    "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-CIDR-RU-checked.txt"
 ]
 
 # ------------------ Утилиты ------------------
