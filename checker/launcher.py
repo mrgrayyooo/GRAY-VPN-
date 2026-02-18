07d9298099621df4be5c77f1fa098608a1bf4143
mrgrayyooo <jf9kzggkxv@privaterelay.appleid.com>
2026-02-18 20:33:43 +0200

    Update launcher.py

--- checker/launcher.py
+++ checker/launcher.py
@@ -20,7 +20,7 @@ XRAY_PATH = "./core/xray"
 MAX_CHECK = 3000
 FINAL_LIMIT = 50
 CONCURRENCY = 15
-SPEED_LIMIT = float(os.getenv("SPEED_LIMIT", 1))
+SPEED_LIMIT = float(os.getenv("SPEED_LIMIT", 0.4))
 TEST_URL = "https://speed.cloudflare.com/__down?bytes=10000000"
 TCP_PING_TIMEOUT = 3
 MMDB_URL = "https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-Country.mmdb"
@@ -44,8 +44,10 @@ logger = logging.getLogger("checker")
 SOURCES = [
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/Vless-Reality-White-Lists-Rus-Mobile.txt",
     "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-SNI-RU-all.txt",
-    "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-CIDR-RU-all.txt"
-    
+    "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/WHITE-CIDR-RU-all.txt",
+    "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/BLACK_VLESS_RUS_mobile.txt",
+    "https://raw.githubusercontent.com/igareck/vpn-configs-for-russia/refs/heads/main/Vless-Reality-White-Lists-Rus-Mobile-2.txt",
+    "https://raw.githubusercontent.com/Danialsamadi/v2go/refs/heads/main/Splitted-By-Country/PL.txt"
 ]
 
 # ------------------ Утилиты ------------------
